989f6c6d40903bb82177f311da96e1c6bd7f1b78
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-04-09 22:01:49 +0200

    hello react native end

--- HelloReactNative/4-Navigation and ScreenChange/src/screens/CoursesScreen.js
+++ HelloReactNative/4-Navigation and ScreenChange/src/screens/CoursesScreen.js
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import React from 'react';
+import { FlatList, StyleSheet, Text } from "react-native";
+import React from "react";
 
 export default function CoursesScreen() {
   const courses = [
-    { name: 'Angular', id: 1 },
-    { name: 'React Js', id: 2 },
-    { name: 'C#', id: 3 },
-    { name: 'C Programlama', id: 4 },
-    { name: 'Bootstrap', id: 5 },
+    { name: "Angular", id: 1 },
+    { name: "React Js", id: 2 },
+    { name: "C#", id: 3 },
+    { name: "C Programlama", id: 4 },
+    { name: "Bootstrap", id: 5 },
   ];
   return (
     <FlatList
@@ -25,7 +25,7 @@ export default function CoursesScreen() {
 const styles = StyleSheet.create({
   content: {
     fontSize: 20,
-    backgroundColor: 'yellow',
+    backgroundColor: "yellow",
     marginVertical: 10,
     padding: 20,
   },
