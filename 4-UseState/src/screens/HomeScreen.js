fc4a0481f49dd4dd73cc455f0e65e8b15e124115
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-04-12 01:04:58 +0200

    useState 1.class end

---
+++ 4-UseState/src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Ana Ekran</Text>
+      <Button
+        title="Kurslarım"
+        onPress={() => navigation.navigate("Kurslarım")}
+      />
+      <Button
+        title="Kurs Bilgilerim"
+        onPress={() => navigation.navigate("KursBilgilerim")}
+      />
+      <Button
+        title="Sayac Uygulamasi"
+        onPress={() => navigation.navigate("Sayac")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
